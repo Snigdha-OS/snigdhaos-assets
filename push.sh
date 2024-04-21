41beda10c8cb617c57b294dea5895901e10b8383
Eshan Roy (Eshanized) <m.eshanized@gmail.com>
2024-04-21 11:26:54 +0530

    ⏳ @eshanized continuously updating the repository!

--- push.sh
+++ push.sh
@@ -5,7 +5,7 @@ REPO_URL="https://github.com/Snigdha-OS/snigdhaos-assets.git"
 BRANCH="master"  # or "master" depending on your repository's default branch
 
 # Commit message
-MESSAGE="@eshanized: push via script"
+MESSAGE="⏳ @eshanized continuously updating the repository!"
 
 # Add all files, commit, and push changes
 git add .
