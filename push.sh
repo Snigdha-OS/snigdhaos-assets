1b03fa567cc58c822ad6d574afd9195eb424f2a6
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2024-05-19 04:00:24 +0530

    feat(upgrade): logo, script have been upgraded

--- push.sh
+++ push.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
-# Set the repository URL and branch
-REPO_URL="https://github.com/Snigdha-OS/snigdhaos-assets.git"
-BRANCH="master"  # or "master" depending on your repository's default branch
+# Author    : Eshan Roy
+# URI       : https://eshanized.github.io
 
-# Commit message
-MESSAGE="⏳ @eshanized continuously updating the repository!"
+# NOTE: If you are on Snigdha OS,
+# you can install commitizen-go with `sudo pacman -S commitizen-go`
+# or `s commitizen-go`. Else you need to install `yay` or `yay-bin`
+# to install commitizen. I have written this script only for *Arch Linux.
 
-# Add all files, commit, and push changes
-git add .
-git commit -m "$MESSAGE"
-git push origin $BRANCH
+# Function to check if Commitizen is installed
+check_commitizen() {
+    if ! pacman -Qq commitizen-go &> /dev/null; then
+        echo "Commitizen is not installed. Please install it using 'yay -S commitizen-go'." >&2
+        exit 1
+    fi
+}
+
+# Function to stage, commit, and push changes
+push_to_github() {
+    git add .
+    git cz
+    git push origin master
+}
+
+# Main Function
+main() {
+    check_commitizen
+    push_to_github
+}
+
+main
